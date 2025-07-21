b6fd3ac1a1c8719453d3f81977abc52d9ba08622
hufsice201820454 <lkh981106@gmail.com>
2025-07-21 16:24:36 +0900

    multi_cahnges

--- basic/intro/echo.py
+++ basic/intro/echo.py
@@ -1,6 +1,12 @@
 def main():
-    user_input = input("문장을 입력하세요: ")
-    print("입력하신 문장은: ", user_input)
+    while True:
+        user_input = input("문장을 입력하세요: ")
+
+        if user_input == "!quit":
+            print("프로그램을 종료합니다. 안녕히 가세요!")
+            break
+
+        print("입력하신 문장은: ", user_input)
 
 if __name__ == "__main__":
     main()
